7ff8eee726845ed366b2ddd2d7e8f83185156a1e
Meatwo310 <72017364+Meatwo310@users.noreply.github.com>
2025-12-21 02:51:41 +0900

    refactor: ItemPaxelの登録方法を変更し、翻訳名とテクスチャ名を自動登録するように
    付随して翻訳のキーを更新

--- src/main/java/io/github/meatwo310/m2paxel/common/item/ItemPaxel.java
+++ src/main/java/io/github/meatwo310/m2paxel/common/item/ItemPaxel.java
@@ -3,6 +3,7 @@ package io.github.meatwo310.m2paxel.common.item;
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Multimap;
+import io.github.meatwo310.m2paxel.M2Paxel;
 import net.minecraft.block.Block;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
@@ -17,8 +18,16 @@ public class ItemPaxel extends ItemPickaxe {
 
     private final double weaponDamage;
 
-    public ItemPaxel(ToolMaterial material) {
+    public ItemPaxel(ToolMaterial material, String itemId) {
+        this(material, M2Paxel.MODID, itemId);
+    }
+
+    public ItemPaxel(ToolMaterial material, String modId, String itemId) {
         super(material);
+
+        this.setUnlocalizedName(modId + "." + itemId);
+        this.setTextureName(modId + ":" + itemId);
+
         this.setMaxDamage(material.getMaxUses() * 4);
         this.setHarvestLevel("axe", material.getHarvestLevel());
         this.setHarvestLevel("shovel", material.getHarvestLevel());
@@ -26,6 +35,8 @@ public class ItemPaxel extends ItemPickaxe {
         this.weaponDamage = 4.0D + material.getDamageVsEntity();
     }
 
+    // thanks mekanism
+    // https://github.com/mekanism/Mekanism/blob/611627519d0c2506028869a00d50488a6ab64fac/src/main/java/mekanism/tools/item/ItemMekanismPaxel.java#L15-L19
     @Override
     public float getDigSpeed(ItemStack stack, Block block, int meta) {
         return block != Blocks.bedrock ? this.efficiencyOnProperMaterial : 1.0F;
--- src/main/java/io/github/meatwo310/m2paxel/common/item/M2PaxelItems.java
+++ src/main/java/io/github/meatwo310/m2paxel/common/item/M2PaxelItems.java
@@ -3,9 +3,9 @@ package io.github.meatwo310.m2paxel.common.item;
 import net.minecraft.item.Item;
 
 public class M2PaxelItems {
-    public static final Item WOODEN_PAXEL = new ItemPaxel(Item.ToolMaterial.WOOD).setUnlocalizedName("m2paxel.wood");
-    public static final Item STONE_PAXEL = new ItemPaxel(Item.ToolMaterial.STONE).setUnlocalizedName("m2paxel.stone");
-    public static final Item IRON_PAXEL = new ItemPaxel(Item.ToolMaterial.IRON).setUnlocalizedName("m2paxel.iron");
-    public static final Item GOLDEN_PAXEL = new ItemPaxel(Item.ToolMaterial.GOLD).setUnlocalizedName("m2paxel.gold");
-    public static final Item DIAMOND_PAXEL = new ItemPaxel(Item.ToolMaterial.EMERALD).setUnlocalizedName("m2paxel.diamond");
+    public static final Item WOODEN_PAXEL = new ItemPaxel(Item.ToolMaterial.WOOD, "wooden_paxel");
+    public static final Item STONE_PAXEL = new ItemPaxel(Item.ToolMaterial.STONE, "stone_paxel");
+    public static final Item IRON_PAXEL = new ItemPaxel(Item.ToolMaterial.IRON, "iron_paxel");
+    public static final Item GOLDEN_PAXEL = new ItemPaxel(Item.ToolMaterial.GOLD, "golden_paxel");
+    public static final Item DIAMOND_PAXEL = new ItemPaxel(Item.ToolMaterial.EMERALD, "diamond_paxel");
 }
