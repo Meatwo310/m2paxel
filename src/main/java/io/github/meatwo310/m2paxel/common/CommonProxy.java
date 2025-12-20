e5540274133e729f17684662326d4b5a67950853
Meatwo310 <72017364+Meatwo310@users.noreply.github.com>
2025-12-21 02:49:07 +0900

    feat: パクセルのレシピ

--- src/main/java/io/github/meatwo310/m2paxel/common/CommonProxy.java
+++ src/main/java/io/github/meatwo310/m2paxel/common/CommonProxy.java
@@ -8,6 +8,8 @@ import cpw.mods.fml.common.registry.GameRegistry;
 import io.github.meatwo310.m2paxel.M2Paxel;
 import io.github.meatwo310.m2paxel.Tags;
 import io.github.meatwo310.m2paxel.common.item.M2PaxelItems;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
 
 public class CommonProxy {
     // preInit "Run before anything else. Read your config, create blocks, items, etc, and register them with the
@@ -26,7 +28,104 @@ public class CommonProxy {
     }
 
     // load "Do your mod setup. Build whatever data structures you care about. Register recipes." (Remove if not needed)
-    public void init(FMLInitializationEvent event) {}
+    public void init(FMLInitializationEvent event) {
+        registerPaxelRecipe();
+    }
+
+    private static void registerPaxelRecipe() {
+//        Item[][] recipes = new Item[][]{
+//            {
+//                M2PaxelItems.WOODEN_PAXEL,
+//                Items.wooden_pickaxe,
+//                Items.wooden_axe,
+//                Items.wooden_shovel,
+//                Items.wooden_sword
+//            },
+//            {
+//                M2PaxelItems.STONE_PAXEL,
+//                Items.stone_pickaxe,
+//                Items.stone_axe,
+//                Items.stone_shovel,
+//                Items.stone_sword
+//            },
+//            {
+//                M2PaxelItems.IRON_PAXEL,
+//                Items.iron_pickaxe,
+//                Items.iron_axe,
+//                Items.iron_shovel,
+//                Items.iron_sword
+//            },
+//            {
+//                M2PaxelItems.GOLDEN_PAXEL,
+//                Items.golden_pickaxe,
+//                Items.golden_axe,
+//                Items.golden_shovel,
+//                Items.golden_sword
+//            },
+//            {
+//                M2PaxelItems.DIAMOND_PAXEL,
+//                Items.diamond_pickaxe,
+//                Items.diamond_axe,
+//                Items.diamond_shovel,
+//                Items.diamond_sword
+//            }
+//        };
+//
+//        for (Item[] recipe : recipes) {
+//            GameRegistry.addShapedRecipe(
+//                // output
+//                new ItemStack(recipe[0]),
+//
+//                // pattern
+//                "W P",
+//                " X ",
+//                "S A",
+//
+//                // ingredients
+//                'X', new ItemStack(Items.stick),
+//                'P', new ItemStack(recipe[1]), // pickaxe
+//                'A', new ItemStack(recipe[2]), // axe
+//                'S', new ItemStack(recipe[3]), // shovel
+//                'W', new ItemStack(recipe[4])  // sword
+//            );
+//        }
+
+        GameRegistry.addShapelessRecipe(
+            new ItemStack(M2PaxelItems.WOODEN_PAXEL),
+            Items.wooden_sword,
+            Items.wooden_pickaxe,
+            Items.wooden_shovel,
+            Items.wooden_axe
+        );
+        GameRegistry.addShapelessRecipe(
+            new ItemStack(M2PaxelItems.STONE_PAXEL),
+            Items.stone_sword,
+            Items.stone_pickaxe,
+            Items.stone_shovel,
+            Items.stone_axe
+        );
+        GameRegistry.addShapelessRecipe(
+            new ItemStack(M2PaxelItems.IRON_PAXEL),
+            Items.iron_sword,
+            Items.iron_pickaxe,
+            Items.iron_shovel,
+            Items.iron_axe
+        );
+        GameRegistry.addShapelessRecipe(
+            new ItemStack(M2PaxelItems.GOLDEN_PAXEL),
+            Items.golden_sword,
+            Items.golden_pickaxe,
+            Items.golden_shovel,
+            Items.golden_axe
+        );
+        GameRegistry.addShapelessRecipe(
+            new ItemStack(M2PaxelItems.DIAMOND_PAXEL),
+            Items.diamond_sword,
+            Items.diamond_pickaxe,
+            Items.diamond_shovel,
+            Items.diamond_axe
+        );
+    }
 
     // postInit "Handle interaction with other mods, complete your setup based on this." (Remove if not needed)
     public void postInit(FMLPostInitializationEvent event) {}
